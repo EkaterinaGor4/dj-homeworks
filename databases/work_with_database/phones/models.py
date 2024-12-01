f638c9e63fba58ee372d0acdc47fc682ab3a6eca
EkaterinaGor4 <147917969+EkaterinaGor4@users.noreply.github.com>
2024-12-01 20:30:15 +0300

    Add files via upload

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -1,6 +1,18 @@
-from django.db import models
+from xmlrpc.client import Boolean
+
+from django.db.models import Model, IntegerField, CharField, DateField, BooleanField, URLField, SlugField
 
 
-class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+class Phone(Model):
+    id = IntegerField(primary_key=True)
+    name = CharField(max_length=200)
+    price = IntegerField()
+    image = URLField()
+    release_date = DateField()
+    lte_exists = BooleanField()
+    slug = SlugField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
